c049c8fb45cc4245cacef5fcbffac172f86d0cb4
Colin Cummins <all.our.august.sundays@gmail.com>
2022-05-18 09:58:29 -0700

    Implemented error handing middleware for all existing routes

--- exercise-api/exercise_controller.mjs
+++ exercise-api/exercise_controller.mjs
@@ -14,23 +14,16 @@ app.use(express.json());
  * Create a new exercise record
  */
 app.post("/exercises",
-    expressAsyncHandler(async (req, res) => {
-        try {
+    expressAsyncHandler(async (req, res, next) => {
             const new_exercise = await exercise.addExercise(req.body);
             res.status(201).type("application/json").send(new_exercise);
-        } catch {
-            console.log("Validation Error")
-            res.status(400).type("application/json").send({ Error: "Invalid request"})
-        }
     }))
-
-
 
 /**
  * Retrieve all exercises.
  */
 app.get("/exercises",
-    expressAsyncHandler(async (req, res) => {
+    expressAsyncHandler(async (req, res, next) => {
         const new_query = await retrieveExercise({});
         res.status(200).type("application/json").send(new_query);
     }))
@@ -39,29 +32,42 @@ app.get("/exercises",
  * Retrieve the exercise corresponding to the ID provided in the URL.
  */
 app.get("/exercises/:_id",
-    expressAsyncHandler(async (req, res) => {
-        try {
+    expressAsyncHandler(async (req, res, next) => {
             const new_query = await retrieveExercise(req.params);
             res.status(200).type("application/json").send(new_query);
-        } catch {
-            res.status(404).type("application/json").send({ Error: "Not found"})
-        }
     }))
 
 /**
  * Update the movie whose id is provided in the path parameter and set
  * its title, year and language to the values provided in the body.
  */
-app.put('/movies/:_id', (req, res) => {
+app.put('/movies/:_id', (req, res, next) => {
     res.status(501).send({ Error: "Not implemented yet" });
 });
 
 /**
  * Delete the movie whose id is provided in the query parameters
  */
-app.delete('/movies/:_id', (req, res) => {
+app.delete('/movies/:_id', (req, res, next) => {
     res.status(501).send({ Error: "Not implemented yet" });
 });
+
+app.use((err, req, res, next) => {
+    console.log("Error: ", err.name)
+    switch (err.name) {
+        case "CastError":
+            res.status(404).type("application/json").send({ Error: "Not found"})
+            break;
+
+        case "ValidationError":
+            console.log("made it")
+            res.status(400).type("application/json").send({ Error: "Invalid request"})
+            break;
+
+        default:
+            console.log("Unhandled Error: ", err.name)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
