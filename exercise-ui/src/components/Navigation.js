9332b1d4c7b3562dc1d0913d3bdfb700811b8235
Colin Cummins <all.our.august.sundays@gmail.com>
2022-08-11 12:58:56 -0700

    Removed login button from nav bar. Need to relocate all login/user code to a new user bar component.

--- exercise-ui/src/components/Navigation.js
+++ exercise-ui/src/components/Navigation.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import LoginButton from "./LoginButton";
 
 function Navigation () {
     return (
         <nav>
             <Link to="/">Home</Link>
             <Link to="/add-exercise">Create Exercise</Link>
-            <><LoginButton>Log In</LoginButton></>
         </nav>
     );
 }
