22742867ce3a07201bc83fa17ff5f2f675079368
Colin Cummins <all.our.august.sundays@gmail.com>
2022-08-16 17:23:17 -0700

    Created ProtectedRoute component that successfully protects most routes, but doesn't work for HomePage for some reason

---
+++ exercise-ui/src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function ProtectedRoute({ children, auth, path, ...rest }) {
+
+    console.log('ProtectedRoute ' + auth);
+    console.log({...rest})
+    return (
+        <Route
+            path={path}
+            render={() => {
+                return auth ? children : <Redirect to='/login' />;
+            }}
+            {...rest}
+        />
+    );
+}
+
+export default ProtectedRoute;
